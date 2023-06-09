fe6c69ca94921a906928e16a635a65c0c1c6a5d3
Ben Smerd <bsmerd@adam.com.au>
2023-06-10 08:48:56 +0930

    add in the db reset query

--- server/db/db.sql
+++ server/db/db.sql
@@ -1,3 +1,8 @@
 -- drop the databse, create it and use it
 -- import the models and create the tables on creation (ones necessary)
 -- can add in seed data just to make it easier to start
+
+-- ? Dropping the database and creating new
+DROP DATABASE IF EXISTS my_library;
+CREATE DATABASE my_library;
+USE my_library;
