62114470b4bcf852cd90263bf487c5499d9e921d
Ben Smerd <bsmerd@adam.com.au>
2023-06-10 17:08:45 +0930

    added in the junction queries to be executed

--- server/models/index.js
+++ server/models/index.js
@@ -1,11 +1,20 @@
 const tables = require("./tables");
+const junctions = require("./junctions");
 const db = require("../connection/connection");
 
 // TODO need to make the tables being imprted an array of queries so can loop throufh and make he queries for them
 
-console.log(tables);
-
 tables.forEach((query) => {
+    db.query(query, (err) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log("Success");
+    });
+});
+
+junctions.forEach((query) => {
     db.query(query, (err) => {
         if (err) {
             console.log(err);
