22023bc9ecfd8909c65dc5d3f076a066b62fe7ee
Ben Smerd <bsmerd@adam.com.au>
2023-06-10 19:14:04 +0930

    added in the root authorRoute path

---
+++ server/routes/api/authorRoute.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+const authorController = require("../../controllers/authorController");
+
+// ? Route '/'
+router.route("/");
+
+module.exports = router;
