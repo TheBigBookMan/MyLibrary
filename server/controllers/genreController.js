13cc2bc8efea09b511c0371558b228cbecf22aec
Ben Smerd <bsmerd@adam.com.au>
2023-06-10 20:27:40 +0930

    added in the createGenre controller for /genre

---
+++ server/controllers/genreController.js
@@ -0,0 +1,25 @@
+const db = require("../connection/connection");
+
+const genreHttp = {
+    createGenre: async (req, res) => {
+        const sql = `INSERT INTO genre (name) VALUES (?)`;
+        const params = req.body.genreName;
+        try {
+            db.query(sql, params, (err, result) => {
+                if (err) {
+                    console.log(err);
+                    res.status(400).json({ error: err.message });
+                    return;
+                }
+                res.json(result);
+            });
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({
+                message: "Could not create genre, please try again.",
+            });
+        }
+    },
+};
+
+module.exports = genreHttp;
