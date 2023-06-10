e333a601a9d2bf9a36bbe6f2785a1f8b6265ba78
Ben Smerd <bsmerd@adam.com.au>
2023-06-10 20:03:58 +0930

    added in the post for the root path of /author

--- server/routes/api/authorRoute.js
+++ server/routes/api/authorRoute.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const authorController = require("../../controllers/authorController");
 
 // ? Route '/'
-router.route("/");
+router.route("/").post(authorController.createAuthor);
 
 module.exports = router;
