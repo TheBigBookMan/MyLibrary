7aa149902ef205a981a1a17504f92d5e956b851b
Ben Smerd <bsmerd@adam.com.au>
2023-06-11 09:13:35 +0930

    added in the getAllLibraries for the path /library

--- server/routes/api/libraryRoute.js
+++ server/routes/api/libraryRoute.js
@@ -2,6 +2,9 @@ const router = require("express").Router();
 const libraryController = require("../../controllers/libraryController");
 
 // ? Route '/'
-router.route("/").post(libraryController.createLibrary);
+router
+    .route("/")
+    .post(libraryController.createLibrary)
+    .get(libraryController.getAllLibraries);
 
 module.exports = router;
