7f5f72b4bb88dedbf4eccc95e31ea1d5ef034128
Ben Smerd <bsmerd@adam.com.au>
2023-06-10 19:05:08 +0930

    update folder structure with utils in client not server

--- server/models/index.js
+++ server/models/index.js
@@ -2,8 +2,6 @@ const tables = require("./tables");
 const junctions = require("./junctions");
 const db = require("../connection/connection");
 
-// TODO need to make the tables being imprted an array of queries so can loop throufh and make he queries for them
-
 tables.forEach((query) => {
     db.query(query, (err) => {
         if (err) {
