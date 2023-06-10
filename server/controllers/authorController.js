cc262ef1e5901fdb52d5f2a394e59a3b798092b2
Ben Smerd <bsmerd@adam.com.au>
2023-06-10 20:14:21 +0930

    added in the getAuthor controller for the author table

--- server/controllers/authorController.js
+++ server/controllers/authorController.js
@@ -21,6 +21,24 @@ const authorHttp = {
             console.log(err);
         }
     },
+    getAuthor: async (req, res) => {
+        const sql = `SELECT authorId, name as authorName FROM author WHERE authorId = ?`;
+        const params = req.params.authorId;
+        try {
+            db.query(sql, params, (err, result) => {
+                if (err) {
+                    res.status(400).json({ error: res.message });
+                    return;
+                }
+                res.json(result);
+            });
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({
+                message: "Could not get author, please try again.",
+            });
+        }
+    },
 };
 
 module.exports = authorHttp;
