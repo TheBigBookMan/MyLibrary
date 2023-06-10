95baae0d88d3749acdaae46d2e323d3770f281dc
Ben Smerd <bsmerd@adam.com.au>
2023-06-11 09:13:14 +0930

    added in the getAllLibraries controller for /library

--- server/controllers/libraryController.js
+++ server/controllers/libraryController.js
@@ -20,6 +20,22 @@ const libraryHttp = {
             res.status(500).json({ message: "Could not create library." });
         }
     },
+    getAllLibraries: async (req, res) => {
+        const sql = `SELECT * FROM library`;
+        try {
+            db.query(sql, (err, result) => {
+                if (err) {
+                    console.log(err);
+                    res.status(400).json({ error: err.message });
+                    return;
+                }
+                res.json(result);
+            });
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({ message: "Could not get all libraries" });
+        }
+    },
 };
 
 module.exports = libraryHttp;
