a748b7b3e2c8bd05945d3e75420082cfeca35c8b
Ben Smerd <bsmerd@adam.com.au>
2023-06-11 11:30:57 +0930

    added in the getAllBooks controller to route /book

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -37,6 +37,22 @@ const bookHttp = {
             res.status(500).json({ message: "Could not create book" });
         }
     },
+    getAllBooks: async (req, res) => {
+        const sql = `SELECT * FROM book`;
+        try {
+            db.query(sql, (err, result) => {
+                if (err) {
+                    console.log(err);
+                    res.status(400).json({ error: err.message });
+                    return;
+                }
+                res.json(result);
+            });
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({ message: "Could not get all books" });
+        }
+    },
 };
 
 module.exports = bookHttp;
