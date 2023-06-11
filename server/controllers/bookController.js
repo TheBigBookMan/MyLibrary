e86e612ba40b6ececbbc3aab95da8bf86d70b4d9
Ben Smerd <bsmerd@adam.com.au>
2023-06-11 11:28:24 +0930

    added in the createBook controller for the path /book

---
+++ server/controllers/bookController.js
@@ -0,0 +1,42 @@
+const db = require("../connection/connection");
+
+const bookHttp = {
+    createBook: async (req, res) => {
+        const sql = `INSERT INTO book (title, starRating, numRatings, image, datePublished, numPages, description, haveRead) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+        const {
+            bookTitle,
+            starRating,
+            numRatings,
+            image,
+            datePublished,
+            numPages,
+            description,
+        } = req.body;
+        const haveRead = false;
+        const params = [
+            bookTitle,
+            starRating,
+            numRatings,
+            image,
+            datePublished,
+            numPages,
+            description,
+            haveRead,
+        ];
+        try {
+            db.query(sql, params, (err, result) => {
+                if (err) {
+                    console.log(err);
+                    res.status(400).json({ error: err.message });
+                    return;
+                }
+                res.json(result);
+            });
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({ message: "Could not create book" });
+        }
+    },
+};
+
+module.exports = bookHttp;
